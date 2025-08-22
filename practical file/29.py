8ca6770b58b3dfab4413ba5017a46eec31207b4c
VISHU LAPPY <akshayj2005>
2025-08-22 19:53:42 +0530

    Your message

---
+++ practical file/29.py
@@ -0,0 +1,4 @@
+num = int(input("Enter a number: "))
+
+temp  = num %10
+print("Last digit of the number is:",temp)
---
+++ practical file/33.py
@@ -0,0 +1,7 @@
+s = "hello"
+i = 0
+
+while i < len(s):
+    print(s[i])
+    i += 1
+
