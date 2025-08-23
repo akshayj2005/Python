41e2c1ffc616b68c5db15352a7923f26aac41cd3
VISHU LAPPY <akshayj2005>
2025-08-23 14:56:05 +0530

    message

--- practical file/26.py
+++ practical file/26.py
@@ -1,2 +1,5 @@
-for i in range(1,8):
-    print(' '*(7-i)+'*'*i)
+input_list = input("Enter integers separated by spaces: ").split()
+int_list = [int(i) for i in input_list]
+print("You entered the following integers:")
+for num in int_list:
+    print(num, end=' ')
