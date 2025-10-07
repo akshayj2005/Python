2598e5bd4bd21aced5e59ee47e218531ed7b991a
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2025-10-07 12:47:29 +0530

    messadw

--- dictionary.py
+++ dictionary.py
@@ -1,52 +1,59 @@
-#create and modify dictionary
-my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
-my_dict['age'] = 31  # Update age
-my_dict['profession'] = 'Engineer'  # Add new key-value pair
-print(my_dict)
-print("\n\n\n\n\n")
+# #create and modify dictionary
+# my_dict = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+# my_dict['age'] = 31  # Update age
+# my_dict['profession'] = 'Engineer'  # Add new key-value pair
+# print(my_dict)
+# print("\n\n\n\n\n")
 
-#accessing values
-print("Name:", my_dict['name'])
-print("Age:", my_dict.get('age'))
-# Removing a key-value pair
-del my_dict['city']
-print("Updated Dictionary:", my_dict)
-print("\n\n\n\n\n")
+# #accessing values
+# print("Name:", my_dict['name'])
+# print("Age:", my_dict.get('age'))
+# # Removing a key-value pair
+# del my_dict['city']
+# print("Updated Dictionary:", my_dict)
+# print("\n\n\n\n\n")
 
-#to demonstrate build in iterators
-my_list = [1, 2, 3, 4, 5]
-my_iterator = iter(my_list)
-print(next(my_iterator))  # Output: 1
-print(next(my_iterator))  # Output: 2
-print("\n\n\n\n\n")
+# #to demonstrate build in iterators
+# my_list = [1, 2, 3, 4, 5]
+# my_iterator = iter(my_list)
+# print(next(my_iterator))  # Output: 1
+# print(next(my_iterator))  # Output: 2
+# print("\n\n\n\n\n")
 
 
-#use iterator in for loop
-my_list = [1, 2, 3, 4, 5]
-my_iter = iter(my_list)
-for value in my_iter:
-    print(value)
-print("\n\n\n\n\n")
+# #use iterator in for loop
+# my_list = [1, 2, 3, 4, 5]
+# my_iter = iter(my_list)
+# for value in my_iter:
+#     print(value)
+# print("\n\n\n\n\n")
 
-#custom iterator class
-class CountDown:
-    def __init__(self, start):
-        self.current = start
+# #custom iterator class
+# class CountDown:
+#     def __init__(self, start):
+#         self.current = start
 
-    def __iter__(self):
-        return self
+#     def __iter__(self):
+#         return self
 
-    def __next__(self):
-        if self.current <= 0:
-            raise StopIteration
-        else:
-            self.current -= 1
-            return self.current + 1
+#     def __next__(self):
+#         if self.current <= 0:
+#             raise StopIteration
+#         else:
+#             self.current -= 1
+#             return self.current + 1
 
         
-#write python code to return a disctory using a function 
-def get_user_profile():
-    profile = {
-        "name":"Alice",
-        "age"
-    }
+# #write python code to return a disctory using a function 
+# def get_user_profile():
+#     profile = {
+#         "name":"Alice",
+#         "age"
+#     }
+
+import modulesd as asdf
+
+print(asdf.add(1,2))
+print(asdf.subtract(25,5))
+
+
