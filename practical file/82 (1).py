2aa1e133df6bc53e3694435a7c2678bf5f6a1af6
Akshayj2005 <bbpsgrakshayj2005@gmail.com>
2025-10-22 00:22:35 +0530

    mfmmf

--- practical file/82 (1).py
+++
@@ -1,5 +0,0 @@
-def add(x,y):
-    return x+y
-def subtract(x,y):
-    return x-y
-print("this code is written and excecuted by MEHAK BHUTANI with ERP 0231BCA063")
--- practical file/83 (1).py
+++
@@ -1,5 +0,0 @@
-import math_utils
-result=math_utils.add(5,3)
-print(result)
-
-print("this code is written and excecuted by MEHAK BHUTANI with ERP 0231BCA063")
